e2bb5c350e3f5497f4aece5060831845e4c90eee
Vince Masten <vmasten@me.com>
2018-07-31 14:43:42 -0700

    more logic editing

--- js/app.js
+++ js/app.js
@@ -20,26 +20,17 @@ function QuizItem (questionText, options, answerRanking, img) {
 }
 
 new QuizItem('What is your biggest fear?', ['Spiders', 'Heights', 'Death'], [2, 0, 1], 'img/path');
-new QuizItem('is this a sample question?', ['option1', 'option2', 'option3'], 'option2', 'img/path');
-new QuizItem('is this a sample question?', ['option1', 'option2', 'option3'], 'option2', 'img/path');
-new QuizItem('is this a sample question?', ['option1', 'option2', 'option3'], 'option2', 'img/path');
-new QuizItem('is this a sample question?', ['option1', 'option2', 'option3'], 'option2', 'img/path');
+new QuizItem('Who is your idol?', ['Barack Obama', 'Dwayne Johnson', 'Taylor Swift'], [1, 2, 0], 'img/path');
+new QuizItem('Where would you go on your dream vacation?', ['Tahiti', 'Paris', 'Tokyo'], [0, 1, 2], 'img/path');
+new QuizItem('What is your favorite genre of video games?', ['Shooter', 'RPG', 'Platformer', 'video games are dumb'], [3, 2, 1, -1], 'img/path');
+new QuizItem('If you were stuck on a desert island, what would be the one piece of media that gets you through it?', ['The movie Fight Club', 'The book Jurassic Park', 'The game The Witcher 3'], [1, 2, 3], 'img/path');
+new QuizItem('Which do you prefer?', ['A small show with a loud band so I can run around in the pit', 'A large arena with a good sound system so I can take the spectacle in', 'Excuse me? All I need is a nice glass of wine and a quiet evening at home'], [2, 1, -1], 'img/path');
+new QuizItem('My dream house...', ['Is gigantic', 'Has a huge yard/is a farm', 'I don\'t really think about it'], [1, 2, 0], 'img/path');
+new QuizItem('Somebody...', ['stop me!', 'once told me the world is gonna roll me', 'told me that you had a boyfriend that looked like a girlfriend that I had in February of last year'], [0, 1, 2], 'img/path');
+new QuizItem('What are you doing RIGHT NOW?', ['Taking this stupid quiz', 'Getting annoyed at all these questions', 'Wondering how my life led me to this'], [2, 1, 0], 'img/path');
+new QuizItem('Anything else we should know?', ['why are you asking me this', 'What is this, a job interview?', 'No, I think you\'ve pretty much covered it.'], [1, 0, 2], 'img/path');
 var quiz1 = new Quiz(quizItems);
 quizItems = [];
-
-//prototype score tracker
-//should eventually be modified to use an event listener
-// function correctAnswer(quiz) {
-//   //for loop to iterate through questions goes here
-//   if (quiz.quizItems[0].options[1] === quiz.quizItems[0].answer) {
-//     quiz.score++;
-//   }
-
-// }
-
-
-
-// Kris's work below
 
 function renderStart() {
   var q1Div = document.createElement('div');
@@ -80,10 +71,12 @@ function renderQuiz() {
   for (var j = 0; j < quiz1.quizItems[quizQuestion].options.length; j++) {
     var divEl = document.createElement('p');
     var inputEl = document.createElement('INPUT');
+    
     inputEl.setAttribute('type', 'radio');
     inputEl.setAttribute('id', 'button' + j);
     inputEl.setAttribute('value', quiz1.quizItems[quizQuestion].answerRanking[j]);
     inputEl.setAttribute('name', quiz1.quizItems[quizQuestion].questionText);
+    
     var createLabel = document.createElement('label');
     createLabel.setAttribute('for', 'button' + j);
     createLabel.textContent = quiz1.quizItems[quizQuestion].options[j];
@@ -100,49 +93,27 @@ function renderQuiz() {
   newDiv.appendChild(submitEl);
   submitEl.addEventListener('click', nextQuestion);
 
-  
-  
-  //working prototype
-
-  // for (var i in quiz1.quizItems) {
-  //   var newDiv = document.createElement('div');
-  //   var createH3 = document.createElement('h3');
-  //   createH3.textContent = quiz1.quizItems[i].questionText;
-  //   newDiv.appendChild(createH3);
-  //   var createOptions = document.getElementById('INPUT');
-  //   // for (var j = 0; j < quiz1.quizItems[i].options.length; j++) {
-  //   var createOptions = document.createElement('INPUT');
-  //   createOptions.setAttribute('type', 'radio');
-  //   createOptions.setAttribute('id', 'button' + j);
-  //   createOptions.setAttribute('name', quiz1.quizItems[i].questionText);
-  //   var createLabel = document.createElement('label');
-  //   createLabel.setAttribute('for', 'button' + j);
-  //   createLabel.textContent = quiz1.quizItems[i].options[j];
-  //   newDiv.appendChild(createLabel);
-  //   newDiv.appendChild(createOptions);
-  // }
   document.getElementById('main').appendChild(newDiv);
+  submitEl.focus();
 
-  // document.getElementById('main').
 }
 
-function nextQuestion(score) {
-  if (document.getElementById('button0').checked) {
-    quiz1.score += parseInt(document.getElementById('button0').value);
-  }
-  else if (document.getElementById('button1').checked) {
-    quiz1.score += parseInt(document.getElementById('button1').value);
-  }
-  else {
-    quiz1.score += parseInt(document.getElementById('button2').value);
-  }
-  console.log(quiz1.score);
+function nextQuestion() {
+
   document.getElementById('question' + submitID).style.display = 'none';
   submitID++;
   quizQuestion++;
-  renderQuiz();
+  if (quizQuestion < quiz1.quizItems.length)
+  {
+    renderQuiz();
+  }
+  else {
+    var scoreAdder = document.getElementsByTagName('input');
+    for (var i in scoreAdder) {
+      if (scoreAdder[i].checked)
+        quiz1.score += parseInt(scoreAdder[i].value);
+    }
+  }
 }
-
-
 
 renderStart();
