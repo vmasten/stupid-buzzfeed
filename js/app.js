51d16f7f6c848a27b0ac297a6e6c4eba9dc114e3
canned-ice <torenesk@gmail.com>
2018-08-01 11:21:21 -0700

    adding changes after resolving issues with staging

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
 'use strict'; //always
 
-var quizzes = [];
-var quizzesPlayed = [];
+var quizzes = []; // variable that holds all quizzes
+var quizzesPlayed = []; // variable that holds all quizzes played for localStorage
 var quizId = [document.getElementById('quiz1'), document.getElementById('quiz2'), document.getElementById('quiz3')];
+// variable that gives a quiz id to pass into html
+var quizItems = []; // variable that holds each individual question + answer array
+var quizQuestion = 0; // variable holding which question user is currently on
+var submitID = 0; // variable that increments after each quiz is taken
 
-// Vince's work below
-var quizItems = [];
-var quizQuestion = 0;
-var submitID = 0;
+var Quiz = function(name, quizItems, results) { // object constructor for each Quiz
+  this.name = name; // name of quiz
+  this.score = 0; // user's current score
+  this.quizItems = quizItems; // quiz questions
+  this.results = results; // results of quiz after score factored in
 
-var Quiz = function(name, quizItems, results) {
-  this.name = name;
-  this.score = 0;
-  this.quizItems = quizItems;
-  this.results = results;
-
-  quizzes.push(this);
+  quizzes.push(this); // pushes quiz instance into var quizzes
 };
 
-function QuizItem(questionText, options, answerRanking, img) {
-  this.questionText = questionText;
-  this.options = options;
-  this.answerRanking = answerRanking;
-  this.img = img;
+function QuizItem(questionText, options, answerRanking, img) { // object constructor for each quiz question
+  this.questionText = questionText;  // question text
+  this.options = options; // answer options
+  this.answerRanking = answerRanking; // assigned score for each answer
+  this.img = img; // img path (if available)
 
-  quizItems.push(this);
+  quizItems.push(this); // pushes all questions into var quizItems
 }
 
+// QUIZ ONE
 new QuizItem('What is your biggest fear?', ['Spiders', 'Heights', 'Death'], [4, 0, 2], '');
 new QuizItem('Who is your idol?', ['Barack Obama', 'Dwayne Johnson', 'Taylor Swift'], [2, 3, 0], '');
 new QuizItem('Where would you go on your dream vacation?', ['Tahiti', 'Paris', 'Tokyo'], [0, 1, 2], '');
@@ -38,8 +38,9 @@ new QuizItem('Somebody...', ['stop me!', 'once told me the world is gonna roll m
 new QuizItem('What are you doing RIGHT NOW?', ['Taking this stupid quiz', 'Getting annoyed at all these questions', 'Wondering how my life led me to this'], [2, 1, 0], 'imgs/curious.jpg');
 new QuizItem('Anything else we should know?', ['why are you asking me this', 'What is this, a job interview?', 'No, I think you\'ve pretty much covered it.'], [1, 0, 2], '');
 var quiz1 = new Quiz('vQuiz', quizItems, ['oh god how did this get here i\'m bad at computers', 'You\'re the one, Neo', 'You\'re a wizard, Harry']);
-quizItems = [];
+quizItems = []; // resets quizItems for next quiz
 
+// QUIZ TWO
 new QuizItem('What dish sounds most appealing to you on a cold winter evening?', ['Stew', 'Raw onions', 'Ice Cream'], [3, 1, 2], '');
 new QuizItem('When ordering food, what\'s your response to \"how spicy?\"', ['No spice, please', '2 chilis', 'Bring on the ghost peppers!'], [1, 2, 3], '');
 new QuizItem('Fruits, vegetables, or meat?', ['Veggies', 'Fruits', 'I am a true carnivore, so pass the meat!'], [2, 1, 3], '');
@@ -51,27 +52,27 @@ new QuizItem('Would you rather cook dinner, order delivery, or eat out?', ['Cook
 new QuizItem('Do you prefer sweet, salty, or spicy?', ['spicy', 'salty', 'sweet'], [3, 2, 1], '');
 new QuizItem('Choose the delicacy you are forced to eat:', ['Surströmming (fermented herring)', 'Balut (boiled developed bird egg)', 'Kiviak (Auk bird stuffed and fermented in seal carcass)'], [2, 1, 3], '');
 var quiz2 = new Quiz('cQuiz', quizItems, ['low result', 'medium result', 'high result']);
-quizItems = [];
+quizItems = []; // resets quizItems for next quiz
 
-var startButton = document.createElement('button');
-function renderStart() {
-  if (localStorage.userName) {
+var startButton = document.createElement('button'); // button that starts the quiz when clicked
+function renderStart() { // function that starts the quiz flow
+  if (localStorage.userName) { // if user has already been to site, skip asking their name
     // do nothing
   } else {
-    var userNameInput = prompt('Hi! What is your name?');
-    localStorage.setItem('userName', userNameInput);
+    var userNameInput = prompt('Hi! What is your name?'); // user's first time, ask for their name via pop-up
+    localStorage.setItem('userName', userNameInput); // saving user name into localStorage
   }
 
-  if (localStorage.quizzesPlayedArray) {
-    quizzesPlayed = JSON.parse(localStorage.getItem('quizzesPlayedArray'));
+  if (localStorage.quizzesPlayedArray) { // if there is data in localStorage, parse it into quizzedPlayedArray for results to feed off of
+    quizzesPlayed = JSON.parse(localStorage.getItem('quizzesPlayedArray')); 
   }
 
-  for (var i = 0; i < quizzes.length; i++) {
+  for (var i = 0; i < quizzes.length; i++) { // loop to create html elements for the quiz
     var qh3 = document.createElement('h3');
-    qh3.textContent = quizzes[i].name;
+    qh3.textContent = quizzes[i].name; // h3 w/quiz name
     quizId[i].appendChild(qh3);
     var qPic = document.createElement('img');
-    qPic.src = 'http://via.placeholder.com/300x100';
+    qPic.src = 'http://via.placeholder.com/300x100'; // img placeholder
     quizId[i].appendChild(qPic);
     var quizText = document.createElement('p');
     quizText.textContent = 'Click Here!';
